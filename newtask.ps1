1112ca9b5db6297ddacb50946e45cc49433e98aa
horim00 <mho@pobox.com>
2023-03-21 08:39:18 +0900

    Update newtask.ps1
    memo.txt renamed to patent number + memo.txt

--- newtask.ps1
+++ newtask.ps1
@@ -1,7 +1,10 @@
 param([string]$patnum)
 
-$patnum
+$memotxt=$patnum + "memo.txt"
 
 md C:\Users\mhori\Documents\test-folders\$patnum
 
-Copy-item -Path "C:\Users\mhori\template-folder\*.xls" -Destination "C:\Users\mhori\Documents\test-folders\$patnum\"  -Recurse
+copy-item -Path "C:\Users\mhori\template-folder\*.xls" -Destination "C:\Users\mhori\Documents\test-folders\$patnum\"  -Recurse
+copy-item -Path "C:\Users\mhori\template-folder\*.txt" -Destination "C:\Users\mhori\Documents\test-folders\$patnum\"  -Recurse
+
+rename-item -Path "C:\Users\mhori\Documents\test-folders\$patnum\memo.txt" -NewName  $memotxt
