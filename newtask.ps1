c7865ec8eb9d45c30582b561de38ea8ae08ef4a7
horim00 <mho@pobox.com>
2023-03-21 10:28:31 +0900

    beta release

--- newtask.ps1
+++ newtask.ps1
@@ -1,10 +1,13 @@
 param([string]$patnum)
 
-$memotxt=$patnum + "memo.txt"
+$memo="memo.txt"
+$patnummemotxt=$patnum+$memo
+$sourcedir = "C:\Users\mhori\template-folder\"
+$targetdir  = "C:\Users\mhori\Documents\test-folders\"+$patnum+"\"
 
-md C:\Users\mhori\Documents\test-folders\$patnum
+mkdir $targetdir
 
-copy-item -Path "C:\Users\mhori\template-folder\*.xls" -Destination "C:\Users\mhori\Documents\test-folders\$patnum\"  -Recurse
-copy-item -Path "C:\Users\mhori\template-folder\*.txt" -Destination "C:\Users\mhori\Documents\test-folders\$patnum\"  -Recurse
+copy-item -Path $sourcedir"*.xls" -Destination $targetdir  -Recurse
+copy-item -Path $sourcedir"*.txt" -Destination $targetdir  -Recurse
 
-rename-item -Path "C:\Users\mhori\Documents\test-folders\$patnum\memo.txt" -NewName  $memotxt
+rename-item -Path $targetdir$memo  -NewName  $patnummemotxt
